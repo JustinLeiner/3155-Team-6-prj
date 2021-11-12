af8e2fbee03a10720a6e0243c822ef0beba8521a
Justin Leiner <58528320+JustinLeiner@users.noreply.github.com>
2021-11-12 15:38:43 -0500

    simplified version

--- prj_flask.py
+++ prj_flask.py
@@ -25,111 +25,31 @@ with app.app_context():
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
 
-# login page #
+# Home page #
 @app.route('/')
-@app.route('/index')
+@app.route('/home', methods = ['GET', 'POST'])
 def index():
-    a_user = db.session.query(User).filter_by(email='jleiner1@uncc.edu').one()
-    return render_template('login.html', user = a_user)
-
-# home page #
-@app.route('/home')
-def home():
-    a_user = db.session.query(User).filter_by(email='jleiner1@uncc.edu').one()
-    return render_template('home.html', user = a_user)
+    return render_template('home.html', new_post = new_post)
 
 # new post template #
-@app.route('/new_question')
-def new_question():
-    a_user = db.session.query(User).filter_by(email='jleiner1@uncc.edu').one()
-    new_question = db.session.query(Note).all()
-    return render_template('new_question.html', notes = new_question, user = a_user)
+@app.route('/new_question', methods = ['GET', 'POST'])
+def new_post():
+    if request.method== 'POST':
 
-# new post submit
-@app.route('/new_question/new', methods=['GET', 'POST'])
-def new_note():
+        title = request.form['questionTitle']
 
-    if request.method == 'POST':
-        
-        # Get title data
-        title = request.form['title']
+        text = request.form['newQuestion']
 
-        # Get note data
-        text = request.form['noteText']
+        new_record = Note(title, text)
 
-        # Create date stamp
-        from datetime import date
-        today = date.today()
-
-        # Date format
-        today = today.strftime("%m-%d-%Y")
-
-        new_record = Note(title, text, today)
         db.session.add(new_record)
         db.session.commit()
 
-        return redirect(url_for('home'))
+        return redirect(url_for('home.html'))
     else:
 
-        a_user = db.session.query(User).filter_by(email='jleiner1@uncc.edu').one()
-        return render_template('new_question.html', user = a_user)
+        return render_template('new_question.html')
 
-
-
-@app.route('/notes')
-def get_notes():
-    a_user = db.session.query(User).filter_by(email='jleiner1@uncc.edu').one()
-    my_notes = db.session.query(Note).all()
-
-    return render_template('notes.html', notes = my_notes, user = a_user)
-
-@app.route('/notes/<note_id>')
-def get_note(note_id):
-    
-    a_user = db.session.query(User).filter_by(email='jleiner1@uncc.edu').one()
-   
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-
-    return render_template('note.html', note = my_note, user = a_user)
-
-@app.route('/notes/edit/<note_id>', methods=['GET', 'POST'])
-def update_note(note_id):
-
-    # check method used for request
-    if request.method == 'POST':
-
-        #get title data
-        title = request.form['title']
-
-        #get note data
-        text = request.form['noteText']
-        note = db.session.query(Note).filter_by(id=note_id).one()
-
-        #update note data
-        note.title = title
-        note.text = text
-        
-        #update note in DB
-        db.session.add(note)
-        db.session.commit()
-
-        return redirect(url_for('get_notes'))
-    else:
-
-        a_user = db.session.query(User).filter_by(email='jleiner1@uncc.edu').one()
-
-        my_note = db.session.query(Note).filter_by(id=note_id).one()
-
-        return render_template('new.html', note = my_note, user = a_user)
-
-@app.route('/notes/delete/<note_id>', methods=['POST'])
-def delete_note(note_id):
-
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-    db.session.delete(my_note)
-    db.session.commit()
-
-    return redirect(url_for('get_notes'))
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
