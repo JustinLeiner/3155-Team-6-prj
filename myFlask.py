8791a9d7aa33e7392882c03284bc350a8462ca1a
sstrilk1 <89851268+sstrilk1@users.noreply.github.com>
2021-11-12 21:09:25 -0500

    Add files via upload
    Worked for me up until the update into the database (What I mean by that is when I hit edit and get all the stored variables from that question in edit_selected_question, when I change something and click "Save" it gives me a sqlachemy.exc error.)

--- myFlask.py
+++ myFlask.py
@@ -23,22 +23,13 @@ with app.app_context():
 # Home page #
 @app.route('/')
 @app.route('/home', methods = ['GET', 'POST'])
-@app.route('/home/', methods = ['GET', 'POST'])
 def index():
-    a_user = db.session.query(User).filter_by(email='jleiner1@uncc.edu').one()
+    a_user = db.session.query(User).filter_by(email='sstrilk1@uncc.edu').one()
 
     user_posts = db.session.query(Note).all()
 
    # print(new_post)
     return render_template('home.html', posts = user_posts, user=a_user)
-
-@app.route('/<post_id>')
-def get_post(post_id):
-    a_user = db.session.query(User).filter_by(email='jleiner1@uncc.edu').one()
-
-    user_post = db.session.query(Note).filter_by(id=post_id).one()
-
-    return render_template('selected_question.html', posts = user_post, user=a_user)
 
 # new post template #
 @app.route('/new_question', methods = ['GET', 'POST'])
@@ -63,44 +54,27 @@ def new_post():
 
         return redirect(url_for('index'))
     else:
-        a_user = db.session.query(User).filter_by(email='jleiner1@uncc.edu').one()
+        a_user = db.session.query(User).filter_by(email='sstrilk1@uncc.edu').one()
         return render_template('new_question.html', user=a_user)
 
-@app.route('/home/edit/<post_id>', methods=['GET', 'POST'])
-def update_post(post_id):
-    #check method used for request
+@app.route('/edit/<note_id>', methods = ['GET', 'POST'])
+def update_post(note_id):
     if request.method == 'POST':
-        # get title data
-        title = request.form['questiontitle']
-        # get post data
-        text = request.form['newquestion']
-        post = db.session.query(Note).filter_by(id=post_id).one()
-        # update post data
-        post.title = title
-        post.text = text
-        # update post in DB
-        db.session.add(post)
+        #get title data
+        title = request.form['title']
+        #get note data
+        text = request.form['noteText']
+        note = db.session.query(Note).filter_by(id=note_id).one()
+        #update note data
+        note.title = title
+        note.text = text
+        #update note in DB
+        db.session.add(note)
         db.session.commit()
-
-        return redirect(url_for('index'))
+        return redirect(url_for('get_notes'))
     else:
-        #GET request - show new post form to edit post
-        #retrieve user from database
-        a_user = db.session.query(User).filter_by(email='jleiner1@uncc.edu').one()
-
-        #retrieve post from database
-        my_post = db.session.query(Note).filter_by(id=post_id).one()
-
-        return render_template('edit_selected_question.html', post=my_post, user=a_user)
-
-@app.route('/home/selected_question/delete/<post_id>', methods=['POST'])
-@app.route('/home/delete/<post_id>', methods=['POST'])
-def delete_post(post_id):
-    #retrieve post from database
-    user_post = db.session.query(Note).filter_by(id=post_id).one()
-    db.session.delete(user_post)
-    db.session.commit()
-
-    return redirect(url_for('index'))
+        a_user = db.session.query(User).filter_by(email='sstrilk1@uncc.edu').one()
+        my_post = db.session.query(Note).filter_by(id=note_id).one()
+        return render_template('edit_selected_question.html', note=my_post, user=a_user)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
