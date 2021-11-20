f8b23c0d63633420fccd42e2332e5104b9b12798
Spencer Jarvis <sjarvi13@uncc.edu>
2021-11-19 21:50:22 -0500

    Revert "Replies Now Work."
    This reverts commit 967ab703c9c20a4d7d765296cf7d546580921d78.

--- models.py
+++ models.py
@@ -6,8 +6,8 @@ class Note(db.Model):
     title = db.Column("title", db.String(200))
     text = db.Column("text", db.String(100))
     date = db.Column("date", db.String(50))
+
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    comments= db.relationship("Comment", backref="note", cascade="all, delete-orphan", lazy=True)
 
     def __init__(self, title, text, date, user_id):
         self.title = title
@@ -21,8 +21,7 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
     notes = db.relationship("Note", backref="user", lazy=True)
-    comments = db.relationship("Comment", backref="user", lazy=True)
-    
+
     def __init__(self, email, password):
         self.email = email
         self.password = password
