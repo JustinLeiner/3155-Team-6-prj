2755ab1dbd38d1514dbfd5c9c2aa54afd851a97c
amcken22 <89826670+amcken22@users.noreply.github.com>
2021-11-29 15:14:57 -0500

    Update like a post.py

--- like a post.py
+++ like a post.py
@@ -2,14 +2,14 @@
 @app.route('/like/<question_id>/<action>')
 def like(post_id, action):
     if session.get('user'):
-        my_question = db.session.query(Question).filter_by(id=post_id).one()
+        post = db.session.query(Question).filter_by(id=post_id).one()
         user = db.session.query(User).filter_by(user_id=session['user_id'])
 
         if action == 'like':
-            user.like_question(my_question)
+            user.like_question(post)
             db.session.commit()
         if action == 'unlike':
-            user.unlike_question(my_question)
+            user.unlike_question(post)
             db.session.commit()
 
        return render_template('selected_question.html', post=user_post, user=session['user_id'], form=form)
