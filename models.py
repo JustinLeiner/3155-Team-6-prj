b8e00403331f49740b61c8a44b559de6a43035e1
amcken22 <89826670+amcken22@users.noreply.github.com>
2021-12-03 01:27:48 -0500

    Update models.py

--- models.py
+++ models.py
@@ -5,11 +5,15 @@ class Note(db.Model):
     title = db.Column("title", db.String(200))
     text = db.Column("text", db.String(100))
     date = db.Column("date", db.String(50))
+    likes = db.Column("like", db.Integer)
+    dislikes = db.Column("dislike", db.Integer)
 
-    def __init__(self, title, text, date):
+    def __init__(self, title, text, date, likes, dislikes):
         self.title = title
         self.text = text
         self.date = date
+        self.likes = likes
+        self.dislikes = dislikes
 
 class User(db.Model):
     id = db.Column("id", db.Integer, primary_key = True)
