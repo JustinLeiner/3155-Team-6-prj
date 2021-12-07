ef8a0bd3bf773ba70d7a152a4933cf0daeda387e
amcken22 <89826670+amcken22@users.noreply.github.com>
2021-12-06 22:35:00 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,25 +1,50 @@
 from database import db
+import datetime
 
 class Note(db.Model):
     id = db.Column("id", db.Integer, primary_key = True)
     title = db.Column("title", db.String(200))
     text = db.Column("text", db.String(100))
     date = db.Column("date", db.String(50))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    comments= db.relationship("Comment", backref="note", cascade="all, delete-orphan", lazy=True)
     likes = db.Column("like", db.Integer)
     dislikes = db.Column("dislike", db.Integer)
 
-    def __init__(self, title, text, date, likes, dislikes):
+    def __init__(self, title, text, date, user_id, liked, dislikes):
         self.title = title
         self.text = text
         self.date = date
+        self.user_id = user_id
         self.likes = likes
         self.dislikes = dislikes
 
 class User(db.Model):
     id = db.Column("id", db.Integer, primary_key = True)
-    name = db.Column("name", db.String(100))
     email = db.Column("email", db.String(100))
+    password = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    notes = db.relationship("Note", backref="user", lazy=True)
+    comments = db.relationship("Comment", backref="user", lazy=True)
+    darkmode = db.Column("darkmode", db.Boolean, nullable=False, default=False)
 
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email 
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+        self.registered_on = datetime.date.today()
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    content = db.Column(db.VARCHAR, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("note.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, content, post_id, user_id):
+        self.date_posted = datetime.date.today()
+        self.content = content
+        self.post_id = post_id
+        self.user_id = user_id
+        self.likes = likes
+        self.dislikes = dislikes
+        
